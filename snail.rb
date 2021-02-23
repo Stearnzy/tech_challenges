3d7aff938acf821684af2c97dce7952869713d7f
Zach Stearns <zach.stearns92@gmail.com>
2021-02-23 09:56:57 -0700

    Add oneline solution

--- snail.rb
+++ snail.rb
@@ -33,3 +33,7 @@ print result_array.flatten.uniq
 
 # => [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]
 
+
+# def snail(array)
+#   array.empty? ? [] : array.shift + snail(array.transpose.reverse)
+# end
